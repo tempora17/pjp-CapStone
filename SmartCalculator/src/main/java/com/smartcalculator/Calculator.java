f2270b6183ebfe6f7f00a7ceb72abf22c401670c
Aniket Singh <aniket170195@gmail.com>
2026-08-21 17:17:08 +0530

    refactor(): Added Google Java Format

--- SmartCalculator/src/main/java/com/smartcalculator/Calculator.java
+++ SmartCalculator/src/main/java/com/smartcalculator/Calculator.java
@@ -3,75 +3,70 @@ package com.smartcalculator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * Perform mathematical calculation for Smart Calculator
- */
+/** Perform mathematical calculation for Smart Calculator */
 public class Calculator {
 
-    public static final Logger LOG = LoggerFactory.getLogger(Calculator.class);
+  public static final Logger LOG = LoggerFactory.getLogger(Calculator.class);
 
-    /**
-     * Calculate the results of the given operation
-     *
-     * @param operation the operation given for the calculation
-     * @return the result of the calculation
-     */
-    public static double calculate(Operation operation){
-        double firstNumber = operation.getFirstNumber();
-        double secondNumber = operation.getSecondNumber();
-        String operator = operation.getOperator();
-        return switch(operator){
-            case "+" -> firstNumber + secondNumber;
-            case "-" -> firstNumber - secondNumber;
-            case "*" -> firstNumber * secondNumber;
-            case "/" -> {
-                if(secondNumber == 0) {
-                    LOG.warn("Cannot divide by zero");
-                    yield Double.NaN;
-                }
-                else{
-                    yield firstNumber / secondNumber;
-                }
-            }
-            case "%" -> {
-                if(secondNumber == 0) {
-                    LOG.warn("Cannot modulo by zero");
-                    yield Double.NaN;
-                }
-                else{
-                    yield firstNumber % secondNumber;
-                }
-            }
-            default -> {
-                LOG.warn("Invalid operator input");
-                yield Double.NaN;
-            }
-        };
-    }
-
-    /**
-     * Calculate the percentage of a value
-     *
-     * @param value the original value
-     * @param percent the percentage to calculate
-     * @return the calculate percentage
-     */
-    public static double percentage(double value, double percent) {
-        return value * percent / 100;
-    }
-
-    /**
-     * Calculate the square root of a number
-     *
-     * @param number the number whose square root is to be calculated
-     * @return the square root of the number. If number is negative then Double.NaN
-     */
-    public static double squareRoot(double number) {
-        if(number < 0) {
-            LOG.warn("Cannot take Square Root of a Negative number");
-            return Double.NaN;
+  /**
+   * Calculate the results of the given operation
+   *
+   * @param operation the operation given for the calculation
+   * @return the result of the calculation
+   */
+  public static double calculate(Operation operation) {
+    double firstNumber = operation.getFirstNumber();
+    double secondNumber = operation.getSecondNumber();
+    String operator = operation.getOperator();
+    return switch (operator) {
+      case "+" -> firstNumber + secondNumber;
+      case "-" -> firstNumber - secondNumber;
+      case "*" -> firstNumber * secondNumber;
+      case "/" -> {
+        if (secondNumber == 0) {
+          LOG.warn("Cannot divide by zero");
+          yield Double.NaN;
+        } else {
+          yield firstNumber / secondNumber;
         }
-        return Math.sqrt(number);
-    }
+      }
+      case "%" -> {
+        if (secondNumber == 0) {
+          LOG.warn("Cannot modulo by zero");
+          yield Double.NaN;
+        } else {
+          yield firstNumber % secondNumber;
+        }
+      }
+      default -> {
+        LOG.warn("Invalid operator input");
+        yield Double.NaN;
+      }
+    };
+  }
 
+  /**
+   * Calculate the percentage of a value
+   *
+   * @param value the original value
+   * @param percent the percentage to calculate
+   * @return the calculate percentage
+   */
+  public static double percentage(double value, double percent) {
+    return value * percent / 100;
+  }
+
+  /**
+   * Calculate the square root of a number
+   *
+   * @param number the number whose square root is to be calculated
+   * @return the square root of the number. If number is negative then Double.NaN
+   */
+  public static double squareRoot(double number) {
+    if (number < 0) {
+      LOG.warn("Cannot take Square Root of a Negative number");
+      return Double.NaN;
+    }
+    return Math.sqrt(number);
+  }
 }
--- SmartCalculator/src/main/java/com/smartcalculator/Main.java
+++ SmartCalculator/src/main/java/com/smartcalculator/Main.java
@@ -1,67 +1,64 @@
 package com.smartcalculator;
 
+import java.util.Scanner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Scanner;
-
-/**
- * Entry point for the Smart Calculator CLI application
- */
+/** Entry point for the Smart Calculator CLI application */
 public class Main {
-    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+  private static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
-    /**
-     * Starts the Smart Calculator application
-     * @param args command line argument
-     */
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        LOG.info("Welcome to My Smart Calculator");
-        LOG.info("Type 'exit' to quit.");
+  /**
+   * Starts the Smart Calculator application
+   *
+   * @param args command line argument
+   */
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    LOG.info("Welcome to My Smart Calculator");
+    LOG.info("Type 'exit' to quit.");
 
-        Operation operation = new Operation();
+    Operation operation = new Operation();
 
-        while(true) {
-            LOG.info("Enter first number (or 'exit'): ");
-            double firstNumberDouble,  secondNumberDouble;
+    while (true) {
+      LOG.info("Enter first number (or 'exit'): ");
+      double firstNumberDouble, secondNumberDouble;
 
-            String firstNumber = sc.nextLine().trim();
-            if(firstNumber.equalsIgnoreCase("exit")) {
-                break;
-            }
+      String firstNumber = sc.nextLine().trim();
+      if (firstNumber.equalsIgnoreCase("exit")) {
+        break;
+      }
 
-            try{
-                firstNumberDouble = Double.parseDouble(firstNumber);
-            } catch (NumberFormatException e){
-                LOG.warn("Please enter a valid First number");
-                continue;
-            }
+      try {
+        firstNumberDouble = Double.parseDouble(firstNumber);
+      } catch (NumberFormatException e) {
+        LOG.warn("Please enter a valid First number");
+        continue;
+      }
 
-            LOG.info("Enter second number: ");
-            String secondNumber = sc.nextLine().trim();
+      LOG.info("Enter second number: ");
+      String secondNumber = sc.nextLine().trim();
 
-            try{
-                secondNumberDouble = Double.parseDouble(secondNumber);
-            } catch (NumberFormatException e){
-                LOG.warn("Please enter a valid Second number");
-                continue;
-            }
+      try {
+        secondNumberDouble = Double.parseDouble(secondNumber);
+      } catch (NumberFormatException e) {
+        LOG.warn("Please enter a valid Second number");
+        continue;
+      }
 
-            LOG.info("Enter operator (+ - * / %): ");
-            String operator = sc.nextLine().trim();
+      LOG.info("Enter operator (+ - * / %): ");
+      String operator = sc.nextLine().trim();
 
-            operation.setFirstNumber(firstNumberDouble);
-            operation.setSecondNumber(secondNumberDouble);
-            operation.setOperator(operator);
+      operation.setFirstNumber(firstNumberDouble);
+      operation.setSecondNumber(secondNumberDouble);
+      operation.setOperator(operator);
 
-            double result = Calculator.calculate(operation);
-            if(!Double.isNaN(result)) {
-                LOG.info(String.format("Result: %.2f%n", result));
-            }
-
-        }
-        LOG.info("GoodBye:)");
-        sc.close();
+      double result = Calculator.calculate(operation);
+      if (!Double.isNaN(result)) {
+        LOG.info(String.format("Result: %.2f%n", result));
+      }
     }
+    LOG.info("GoodBye:)");
+    sc.close();
+  }
 }
